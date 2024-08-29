30e81502748af4d0c32310af204502bd4396d016
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-08-29 16:49:58 +0200

    b

--- includes/_head.php
+++ includes/_head.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="">
     <link class="favicon" rel="icon" type="image/x-icon" href="">
-    <title>Bienvenu sur Memory!</title> // Mettre du PHP
+    <title>Bienvenu sur Memory!</title> <!-- Mettre du PHP -->
 </head>
 
     
