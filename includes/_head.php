823d5aa24422e242a05973ffb6ee28fef45a89fa
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-08-29 16:34:55 +0200

    preparation des includes

---
+++ includes/_head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <link class="favicon" rel="icon" type="image/x-icon" href="">
+    <title>Bienvenu sur Memory!</title> // Mettre du PHP
+</head>
+
+    
---
+++ includes/_header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav class="Navbar">
+        <ul>
+            <li><a href="#Parcours Scolaire">Accueil</a></li>
+            <li><a href="#Experience professionnelle">Profil</a></li>
+            <li><a href="#Competences">Classement</a></li>
+        </ul>
+    </nav>
+</header>
