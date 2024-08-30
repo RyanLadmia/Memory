ca83702d25ed3c493662171130e08d289425c90d
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-08-30 16:04:21 +0200

    Ajout du Favicon

--- includes/_head.php
+++ includes/_head.php
@@ -3,6 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="../assets/css/index.css">
-    <link class="favicon" rel="icon" type="image/x-icon" href="">
-    <title>Memory</title> <!-- Mettre du PHP -->
+    <link class="favicon" rel="icon" type="image/x-icon" href="assets/medias/memory_favicon.jpg">
+    <title>Memory</title>
 </head>
